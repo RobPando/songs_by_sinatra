1bb961588efead237c0d6de88f9eac78af7f0c72
Roberto Pando <roberto_pando@hotmail.com>
2016-10-22 21:57:17 +0200

    configuration changes

--- main.rb
+++ main.rb
@@ -4,58 +4,58 @@ require 'sass'
 require 'slim'
 require './song'
 
-get('/styles.css'){ scss :styles }
+get('/styles.css') { scss :styles }
 
 configure do
-	enable :sessions
-	set :username, 'frank'
-	set :password, 'sinatra'
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
 end
 
-configure :development do 
-	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
-configure :production do 
-	DataMapper.setup(:default, ENV['DATABASE_URL'])
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
 
-get '/' do 
-	slim :home
+get '/' do
+  slim :home
 end
 
 get '/about' do
-	@title = "All About This Website"
-	slim :about
+  @title = "All About This Website"
+  slim :about
 end
 
 get '/contact' do
-	@title = "Contact Us"
-	slim :contact
+  @title = "Contact Us"
+  slim :contact
 end
 
 get '/login' do
-	slim :login
+  slim :login
 end
 
 post '/login' do
-	if params[:username] == settings.username && params[:password] == settings.password
-		session[:admin] = true
-		redirect to('/songs')
-	else
-		@invalid_message = true
-		slim :login
-	end
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/songs')
+  else
+    @invalid_message = true
+    slim :login
+  end
 end
 
 get '/logout' do
-	session.clear
-	redirect to('/login')
+  session.clear
+  redirect to('/login')
 end
 
 not_found do
-	@title = "Page Not Found"
-	slim :not_found
+  @title = "Page Not Found"
+  slim :not_found
 end
 
 
